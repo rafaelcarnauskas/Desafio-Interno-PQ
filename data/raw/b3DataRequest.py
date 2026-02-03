6e6b024e03a531fcd766a6d038df732cd19f303e
Rafael Carnauskas <carnauskasrafael@mail.com>
2026-02-03 16:02:35 -0300

    Atualizações na requisição de dados, acho que agora da pra trabalhar bem com valores de 2010 a 2025

--- data/raw/b3DataRequest.py
+++ data/raw/b3DataRequest.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import yfinance as yf
 
-df = pd.read_csv("precos_b3_202010-2024_adjclose.csv")
+df = pd.read_csv("tickers.csv")
 
-tickers = df.columns.tolist()# salva os nomes dos ativos em uma lista
-tickers.pop(0) # remove a coluna data
+tickers = df["ticker"].tolist()# salva os nomes dos ativos em uma lista
 
 df = pd.DataFrame(tickers)
 df = yf.download(tickers, start='2010-01-01', end='2025-12-31', group_by='ticker', threads=False, auto_adjust=False)
-
-df.to_excel('b3-2010-2025.xlsx')
-
+# obs: os tickers são as colunas e as datas são as linhas
 #Aqui nem todos os downloads foram feitos, talvez refazer o loop para os valores vazios arrume isso
+"""
+incomplete_data = df.columns[df.isna().all()].tolist()
+df_novos = yf.download(incomplete_data, start='2010-01-01', end='2025-12-31', group_by='ticker', threads=False, auto_adjust=False)
+
+df.update(df_novos)"""
+
+df.to_csv('b3-2010-2025.csv')
